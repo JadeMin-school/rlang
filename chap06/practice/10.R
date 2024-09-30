8c49d26573a56cad7e4c20b1b670f72e8ca16a29
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-09-30 12:56:21 +0900

    upload: upload

--- chap06/practice/10.R
+++ chap06/practice/10.R
@@ -3,5 +3,5 @@ library(xlsx)
 setwd('C:/Rworks/practice/chap06/10')
 
 write.xlsx(iris, 'iris.xlsx')
-test = read.xlsx('iris.xlsx', sheetIndex=1)
+test <- read.xlsx('iris.xlsx', sheetIndex=1)
 print(test)
